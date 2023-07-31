affca59f2059ebcc2e387d3ad8d44f5173ba0bb4
Steve Giordano <stephenagiordano@gmail.com>
2023-07-30 22:37:12 -0400

    1am scheduler

--- tasks/cron.js
+++ tasks/cron.js
@@ -2,7 +2,7 @@ import schedule from "node-schedule";
 import tweetScraper from "./jobs/tweet-scraper.js";
 
 const job = () => {
-	schedule.scheduleJob("22 22 * * *", () => {
+	schedule.scheduleJob("0 1 * * *", () => {
 		tweetScraper();
 	});
 };
