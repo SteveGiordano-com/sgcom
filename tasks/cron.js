ba3ef1809bdeeaadec763355cd5810fab16ac47b
Steve Giordano <stephenagiordano@gmail.com>
2023-07-30 22:09:24 -0400

    Updated PF

--- tasks/cron.js
+++ tasks/cron.js
@@ -2,7 +2,7 @@ import schedule from "node-schedule";
 import tweetScraper from "./jobs/tweet-scraper.js";
 
 const job = () => {
-	schedule.scheduleJob("3 22 * * *", () => {
+	schedule.scheduleJob("13 22 * * *", () => {
 		tweetScraper();
 	});
 };
