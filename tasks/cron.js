0a95ad690dc15dae107eb8543e53aaf9ec9f9ebf
Steve Giordano <stephenagiordano@gmail.com>
2023-08-04 18:30:44 -0400

    testing clock

--- tasks/cron.js
+++ tasks/cron.js
@@ -2,7 +2,7 @@ import schedule from "node-schedule";
 import tweetScraper from "./jobs/tweet-scraper.js";
 
 const job = () => {
-	schedule.scheduleJob("27 18 * * *", () => {
+	schedule.scheduleJob("*/2 * * * *", () => {
 		tweetScraper();
 	});
 };
